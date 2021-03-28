5e9eaf88975f3cb8fea14fd7c0863c7eb1380cf7
SHE-43 <69411606+SHE-43@users.noreply.github.com>
2021-03-28 15:45:06 +0100

    Create Modules_to_perfect.py

---
+++ Modules_to_perfect.py
@@ -0,0 +1,17 @@
+
+"""
+from requests import get
+from contextlib import closing
+from datetime import datetime
+import csv
+import os
+import locale
+from datetime import date, timedelta
+import os.path
+import pandas as pd
+import pysftp
+import codecs
+import sys
+
+
+"""
