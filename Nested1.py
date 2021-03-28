3c24da60c04550467d6456ae1e8c0ae1ff4e4f0a
SHE-43 <69411606+SHE-43@users.noreply.github.com>
2021-03-28 15:44:33 +0100

    Update Nested1.py

--- Nested1.py
+++ Nested1.py
@@ -56,20 +56,3 @@ class DictQuery(dict):
 # Tables.
 # Step one: Check type of the object itself.
 
-
-"""
-from requests import get
-from contextlib import closing
-from datetime import datetime
-import csv
-import os
-import locale
-from datetime import date, timedelta
-import os.path
-import pandas as pd
-import pysftp
-import codecs
-import sys
-
-
-"""
